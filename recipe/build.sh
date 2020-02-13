c155839bb090ad2d600efc64f6d5e793a8518130
djkazic <kevincai96@gmail.com>
2020-02-13 12:52:58 -0500

    Add build script for recipe in future

---
+++ recipe/build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+brew install --build-from-source libusb.rb
+cp -r /usr/local/Cellar/libusb/1.0.20/lib/* ../lib/
+install_name_tool -id "@executable_path/../Resources/Plugins/libusb-1.0.0.dylib" ../lib/libusb-1.0.0.dylib
+install_name_tool -id "@executable_path/../Resources/Plugins/libusb-1.0.dylib" ../lib/libusb-1.0.dylib
